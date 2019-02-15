cf2311134e78735daf9d5b69997e385840cbc043
Inho Lee <inhol@uci.edu>
2019-02-14 19:20:03 -0800

    PolyTreeNode passes all tests. Only bfs and dfs remain

--- W1D1/PolyTreeNode/lib/00_tree_node.rb
+++ W1D1/PolyTreeNode/lib/00_tree_node.rb
@@ -2,12 +2,33 @@
 
 class PolyTreeNode
 
-    attr_reader :value, :parent, :children,
+    attr_reader :value, :parent, :children
 
     def initialize(value)
         @value = value
         @parent = nil
         @children = []
     end
+
+    def parent=(node)
+        @parent.children.delete(self) unless @parent == nil
+        @parent = node
+        @parent.children << self unless @parent == nil || @parent.children.include?(self)
+    end
+
+    def add_child(node)
+        @children << node
+        node.parent = self
+    end
+
+    def remove_child(node)
+        raise "Node is not a child" unless @children.include?(node)
+        node.parent = nil
+        @children.delete(node)
+    end
+
+    
+
+
 
 end
